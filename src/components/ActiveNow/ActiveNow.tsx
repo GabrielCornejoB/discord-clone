950dc01fb862c03f8e126d77ccb06b7abf3502bd
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-05-23 11:40:14 -0500

    Ui finished

--- src/components/ActiveNow/ActiveNow.tsx
+++ src/components/ActiveNow/ActiveNow.tsx
@@ -1,6 +1,17 @@
 import s from "./ActiveNow.module.scss";
 
 const ActiveNow = () => {
-  return <aside className={s.activeNow}>ActiveNow</aside>;
+  return (
+    <aside className={s.activeNow}>
+      <h1>Active Now</h1>
+      <div className={s.container}>
+        <h2>It's quiet for now...</h2>
+        <p>
+          When someone starts an activity - like playing a game or hanging out
+          on voice - we'll show it here!
+        </p>
+      </div>
+    </aside>
+  );
 };
 export default ActiveNow;
